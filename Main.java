14792fcf5ac3b852a05239b9503bc33d72521dbc
SyedMuhammadHashir <122075818+SyedMuhammadHashir@users.noreply.github.com>
2023-01-19 21:17:10 -0800

    commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+	public boolean CheckingEvent(int num) {
+		if (num%2 ==0)
+			return true;
+		
+		    return false;
+		    }
+	
+	public boolean CheckingOdd(int num) {
+		if (num%2 !=0)
+			return true;
+		
+		    return false;
+		    }
+	public boolean CheckingPrime(int num){
+		boolean isPrime = true;
+		if (num ==1)
+			isPrime= false;
+		    return false;
+		    }
+
+	public int factorial(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
+                                                                                            
